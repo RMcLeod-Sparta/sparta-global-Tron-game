70825ef5990b9d868edca255f5ca6302db04cf19
Tech-A06 <tech-a06@Tech-A06s-MacBook-Air.local>
2018-05-03 15:40:45 +0100

    fully functioning game

--- jsIndex.js
+++ jsIndex.js
@@ -23,21 +23,17 @@ var direction2 = 1;
 var direction3 = -1;
 var direction4 = 0;
 var int;
-var interval = 100;
+var interval = 110;
 var length = 0;
 var length2 = 0;
-var lagUp = 0;
-var lagSide = 0;
-var lagUp2 = 0;
-var lagSide2 = 0;
-var lagUp3 = 0;
-var lagSide3 = 0;
-var lagUp4 = 0;
-var lagSide4 = 0;
 var p1Dead = false;
 var p2Dead = false;
 var p3Dead = false;
 var p4Dead = false;
+var numPlayes;
+var gameMusic = new Audio("GameMusic.mp3");
+
+
 
 function get(x,y){
   return document.getElementById(x+"-"+y);
@@ -64,183 +60,211 @@ function run(){
 
 function update (){
   updateTrail();
-  if(p1Dead == false){
-    player1();
-  }
-  if(p2Dead == false){
-    player2();
-  }
-  // if(p3Dead == false){
-  //   player3();
-  // }
-  // if(p4Dead == false){
-  //   player4();
-  // }
+    if(p1Dead == false){
+      player1();
+    }
+    if(p2Dead == false){
+      player2();
+    }
+    if(p3Dead == false){
+      player3();
+    }
+    if(p4Dead == false){
+      player4();
+    }
 
- var p1overlaps = ($(".bike1").collision(".trail"));
- if(p1overlaps.length > 0){
-   document.getElementById("result").innerHTML = "Orange Wins!";
-   p1Dead = true;
-   $("td").removeClass("trail")
-   $("td").removeClass("bike1")
- }
- var p1overlaps2 = ($(".bike1").collision( ".trail2"));
- if(p1overlaps2.length > 0){
-   document.getElementById("result").innerHTML = "Orange Wins!";
-   p1Dead = true;
-   $("td").removeClass("trail");
-   $("td").removeClass("bike1");
- }
-
- var p2overlaps = ($(".bike2").collision(".trail"));
- if(p2overlaps.length > 0){
-   document.getElementById("result").innerHTML = "Blue Wins!";
-   p2Dead = true;
-   $("td").removeClass("trail2");
-   $("td").removeClass("bike2");
- }
- var p2overlaps2 = ($(".bike2").collision( ".trail2"));
- if(p2overlaps2.length > 0){
-   document.getElementById("result").innerHTML = "Blue Wins!";
-   p2Dead = true;
-   $("td").removeClass("trail2");
-   $("td").removeClass("bike2");
- }
-
- var draw = ($(".bike1").collision( ".bike2"))
- if(draw.length > 0){
-   document.getElementById("result").innerHTML = "It's A Draw!";
-   p1Dead = true;
-   p2Dead = true;
-   $("td").removeClass("trail");
-   $("td").removeClass("trail2");
-   $("td").removeClass("bike1");
-   $("td").removeClass("bike2");
-//   running = false;
- }
- var draw2 = ($(".bike1").collision( ".bike3"))
- if(draw2.length > 0){
-   document.getElementById("result").innerHTML = "It's A Draw!";
-   p1Dead = true;
-   p3Dead = true;
-   $("td").removeClass("trail");
-   $("td").removeClass("trail3");
-   $("td").removeClass("bike1");
-   $("td").removeClass("bike3");
-//   running = false;
- }
- var draw3 = ($(".bike1").collision( ".bike4"))
- if(draw3.length > 0){
-   document.getElementById("result").innerHTML = "It's A Draw!";
-   p1Dead = true;
-   p4Dead = true;
-   $("td").removeClass("trail");
-   $("td").removeClass("trail4");
-   $("td").removeClass("bike1");
-   $("td").removeClass("bike4");
-//   running = false;
- }
- var draw4 = ($(".bike2").collision( ".bike3"))
- if(draw4.length > 0){
-   document.getElementById("result").innerHTML = "It's A Draw!";
-   p2Dead = true;
-   p3Dead = true;
-   $("td").removeClass("trail2");
-   $("td").removeClass("trail3");
-   $("td").removeClass("bike2");
-   $("td").removeClass("bike3");
-//   running = false;
- }
- var draw5 = ($(".bike2").collision( ".bike4"))
- if(draw5.length > 0){
-   document.getElementById("result").innerHTML = "It's A Draw!";
-   p2Dead = true;
-   p4Dead = true;
-   $("td").removeClass("trail2");
-   $("td").removeClass("trail4");
-   $("td").removeClass("bike2");
-   $("td").removeClass("bike4");
- }
- var draw6 = ($(".bike3").collision( ".bike4"))
- if(draw6.length > 0){
-   document.getElementById("result").innerHTML = "It's A Draw!";
-   p3Dead = true;
-   p4Dead = true;
-   $("td").removeClass("trail3");
-   $("td").removeClass("trail4");
-   $("td").removeClass("bike3");
-   $("td").removeClass("bike4");
-   //running = false;
- }
-
-  if(bikeX == 0 || bikeX == width-1 || bikeY == 0 || bikeY == height-1){
-    document.getElementById("result").innerHTML = "Orange Wins!";
+  function removePlayer1(){
     p1Dead = true;
     $("td").removeClass("trail")
     $("td").removeClass("bike1")
-  }else if(bikeX2 == 0 || bikeX2 == width-1 || bikeY2 == 0 || bikeY2 == height-1){
-    document.getElementById("result").innerHTML = "Blue Wins!";
+  }
+  function removePlayer2(){
     p2Dead = true;
-    $("td").removeClass("trail2");
-    $("td").removeClass("bike2");
+    $("td").removeClass("trail2")
+    $("td").removeClass("bike2")
+    }
+  function removePlayer3(){
+    p3Dead = true;
+    $("td").removeClass("trail3")
+    $("td").removeClass("bike3")
   }
-
+  function removePlayer4(){
+    p4Dead = true;
+    $("td").removeClass("trail4")
+    $("td").removeClass("bike4")
   }
+//The conditions for player 1 losing
+var p1overlaps = ($(".bike1").collision(".trail"));
+if(p1overlaps.length > 0){
+  removePlayer1();
+}
+var p1overlaps2 = ($(".bike1").collision( ".trail2"));
+if(p1overlaps2.length > 0){
+  removePlayer1();
+}
+var p1overlaps3 = ($(".bike1").collision( ".trail3"));
+if(p1overlaps3.length > 0){
+  removePlayer1();
+}
+var p1overlaps4 = ($(".bike1").collision( ".trail4"));
+if(p1overlaps4.length > 0){
+  removePlayer1();
+}
+//The conditions for player 2 losing
+var p2overlaps = ($(".bike2").collision(".trail"));
+if(p2overlaps.length > 0){
+  removePlayer2();
+}
+var p2overlaps2 = ($(".bike2").collision(".trail2"));
+if(p2overlaps2.length > 0){
+  removePlayer2();
+}
+var p2overlaps3 = ($(".bike2").collision(".trail3"));
+if(p2overlaps3.length > 0){
+  removePlayer2();
+}
+var p2overlaps4 = ($(".bike2").collision(".trail4"));
+if(p2overlaps4.length > 0){
+  removePlayer2();
+}
+//The conditions for player 3 losing
+var p3overlaps = ($(".bike3").collision(".trail"));
+if(p3overlaps.length > 0){
+  removePlayer3();
+}
+var p3overlaps2 = ($(".bike3").collision(".trail2"));
+if(p3overlaps2.length > 0){
+  removePlayer3();
+}
+var p3overlaps3 = ($(".bike3").collision(".trail3"));
+if(p3overlaps3.length > 0){
+  removePlayer3();
+}
+var p3overlaps4 = ($(".bike3").collision(".trail4"));
+if(p3overlaps4.length > 0){
+  removePlayer3();
+}
+//The conditions for player 4 losing
+var p4overlaps = ($(".bike4").collision(".trail"));
+if(p4overlaps.length > 0){
+  removePlayer4();
+}
+var p4overlaps2 = ($(".bike4").collision(".trail2"));
+if(p4overlaps2.length > 0){
+  removePlayer4();
+}
+var p4overlaps3 = ($(".bike4").collision(".trail3"));
+if(p4overlaps3.length > 0){
+  removePlayer4();
+}
+var p4overlaps4 = ($(".bike4").collision(".trail4"));
+if(p4overlaps4.length > 0){
+  removePlayer4();
+}
+//The conditons for 2 players losing at once
+var draw = ($(".bike1").collision( ".bike2"))
+if(draw.length > 0){
+  removePlayer1();
+  removePlayer2();
+}
+var draw2 = ($(".bike1").collision( ".bike3"))
+if(draw2.length > 0){
+  removePlayer1();
+  removePlayer3();
+}
+var draw3 = ($(".bike1").collision( ".bike4"))
+if(draw3.length > 0){
+  removePlayer1();
+  removePlayer4();
+}
+var draw4 = ($(".bike2").collision( ".bike3"))
+if(draw4.length > 0){
+  removePlayer2();
+  removePlayer3();
+}
+var draw5 = ($(".bike2").collision( ".bike4"))
+if(draw5.length > 0){
+  removePlayer2();
+  removePlayer4();
+}
+var draw6 = ($(".bike3").collision( ".bike4"))
+if(draw6.length > 0){
+  removePlayer3();
+  removePlayer4();
+}
+//A player hitting the wall
+if(bikeX == 0 || bikeX == width-1 || bikeY == 0 || bikeY == height-1){
+  removePlayer1();
+}else if(bikeX2 == 0 || bikeX2 == width-1 || bikeY2 == 0 || bikeY2 == height-1){
+  removePlayer2();
+}else if(bikeX3 == 0 || bikeX3 == width-1 || bikeY3 == 0 || bikeY3 == height-1){
+  removePlayer3();
+}else if(bikeX4 == 0 || bikeX4 == width-1 || bikeY4 == 0 || bikeY4 == height-1){
+  removePlayer4();
+}
+//Displays the relevant winner depending on who is left.
+if(p2Dead == true && p3Dead == true && p4Dead == true && p1Dead == false){
+    document.getElementById("result").innerHTML = "The Winner Is: Blue";
+    document.getElementById("result").style.color = "deepskyblue";
+    running = false;
+}else if(p1Dead == true && p3Dead == true && p4Dead == true && p2Dead == false){
+    document.getElementById("result").innerHTML = "The Winner Is: Orange";
+    document.getElementById("result").style.color = "darkorange";
+    running = false;
+}else if(p1Dead == true && p2Dead == true && p4Dead == true && p3Dead == false){
+    document.getElementById("result").innerHTML = "The Winner Is: Red";
+    document.getElementById("result").style.color = "red";
+    running = false;
+}else if(p1Dead == true && p2Dead == true && p3Dead == true && p4Dead == false){
+    document.getElementById("result").innerHTML = "The Winner Is: Purple";
+    document.getElementById("result").style.color = "purple";
+    running = false;
+}else if(p1Dead == true && p2Dead == true && p3Dead == true && p4Dead == true){
+    document.getElementById("result").innerHTML = "It's A Draw";
+    document.getElementById("result").style.color = "yellow";
+    running = false;
+}else{
+    document.getElementById("result").innerHTML = "The Winner Is:";
+    document.getElementById("result").style.color = "white";
+}
+}//End of update
 
 function player1(){
-  set((trailX[length]+lagSide), (trailY[length]+lagUp), "trail");
+  set((trailX[length]), (trailY[length]), "trail");
   if(direction == 0){ // up = 0, down = -1, left = 1, right = 2;
-    // lagUp=1;
-    // lagSide=0;
     bikeY--;
   }
   else if(direction == -1){
-    // lagUp=-1;
-    // lagSide=0;
     bikeY++;
   }
   else if(direction == 1){
-    // lagUp=0;
-    // lagSide=1;
     bikeX--;
   }
   else if(direction == 2){
-    // lagUp=0;
-    // lagSide=-1;
     bikeX++;
   }
    setB(bikeX, bikeY, "bike1");
 }
 
 function player2(){
-  set((trailX2[length2]+lagSide2), (trailY2[length2]+lagUp2), "trail2");
+  set((trailX2[length2]), (trailY2[length2]), "trail2");
   if(direction2 == 0){ // up = 0, down = -1, left = 1, right = 2;
-    // lagUp2=1;
-    // lagSide2=0;
     bikeY2--;
   }
   else if(direction2 == -1){
-    // lagUp2=-1;
-    // lagSide2=0;
     bikeY2++;
   }
   else if(direction2 == 1){
-    // lagUp2=0;
-    // lagSide2=1;
     bikeX2--;
   }
   else if(direction2 == 2){
-    // lagUp2=0;
-    // lagSide2=-1;
     bikeX2++;
   }
    setB(bikeX2, bikeY2, "bike2");
 }
 function player3(){
-  set((trailX3[length]+lagSide3), (trailY3[length]+lagUp3), "trail3");
+  set((trailX3[length]), (trailY3[length]), "trail3");
   if(direction3 == 0){ // up = 0, down = -1, left = 1, right = 2;
-    lagUp3=1;
-    lagSide3=0;
     bikeY3--;
   }
   else if(direction3 == -1){
@@ -261,25 +285,17 @@ function player3(){
    setB(bikeX3, bikeY3, "bike3");
 }
 function player4(){
-  set((trailX4[length]+lagSide4), (trailY4[length]+lagUp4), "trail4");
+  set((trailX4[length]), (trailY4[length]), "trail4");
   if(direction4 == 0){ // up = 0, down = -1, left = 1, right = 2;
-    lagUp4=1;
-    lagSide4=0;
     bikeY4--;
   }
   else if(direction4 == -1){
-    lagUp4=-1;
-    lagSide4=0;
     bikeY4++;
   }
   else if(direction4 == 1){
-    lagUp4=0;
-    lagSide4=1;
     bikeX4--;
   }
   else if(direction4 == 2){
-    lagUp4=0;
-    lagSide4=-1;
     bikeX4++;
   }
    setB(bikeX4, bikeY4, "bike4");
@@ -316,7 +332,6 @@ function gameLoop(){
   if(running && !gameOver) {
     update();
   }else if(gameOver){
-  //    console.log("over");
     document.location.reload();
   }
 }
@@ -338,86 +353,73 @@ function createGrid(){
 
 window.addEventListener("keypress", function key(){
   var key = event.keyCode;
-  //if key is W move up
   if(direction != -1 && (key == 119 || key == 87)){
-    //console.log("up");
     direction = 0;
   }
-  //if key is S move down
   else if(direction != 0 && (key == 115 || key == 83)){
-  //  console.log("down");
     direction = -1;
   }
-  //if key is A move left
   else if(direction != 2 && (key == 97 || key == 65)){
-  //  console.log("left");
     direction = 1;
   }
-  //if key is D move right
   else if(direction != 1 && (key == 100 || key == 68)){
-  //  console.log("right");
     direction = 2;
   }
   else if(direction3 != -1 && (key == 84 || key == 116)){
-    //console.log("up");
     direction3 = 0;
   }
-  //if key is S move down
   else if(direction3 != 0 && (key == 71 || key == 103)){
-  //  console.log("down");
     direction3 = -1;
   }
-  //if key is A move left
   else if(direction3 != 2 && (key == 70 || key == 102)){
-  //  console.log("left");
     direction3 = 1;
   }
-  //if key is D move right
   else if(direction3 != 1 && (key == 72 || key == 104)){
-  //  console.log("right");
     direction3 = 2;
   }
   else if(direction2 != -1 && (key == 105 || key == 73)){
-  //  console.log("up2");
     direction2 = 0;
   }
   else if(direction2 != 0 && (key == 107 || key == 75)){
-  //  console.log("down2");
     direction2 = -1;
   }
   else if(direction2 != 2 && (key == 106 || key == 74)){
-  //  console.log("left2");
     direction2 = 1;
   }
   else if(direction2 != 1 && (key == 108 || key == 76)){
-  //  console.log("right2");
     direction2 = 2;
   }
   else if(direction4 != -1 && (key == 123 || key == 91)){
-  //  console.log("up2");
     direction4 = 0;
   }
   else if(direction4 != 0 && (key == 34 || key == 39)){
-  //  console.log("down2");
     direction4 = -1;
   }
   else if(direction4 != 2 && (key == 58 || key == 59)){
-  //  console.log("left2");
     direction4 = 1;
   }
   else if(direction4 != 1 && (key == 124 || key == 92)){
-  //  console.log("right2");
     direction4 = 2;
+  }else if(key == 49){
+    numPlayes = 1;
   }
-  else if((key == 82 || key == 114) && p1Dead == true && p2Dead == true){
-    p1Dead = false;
-    p2Dead = false;
+  else if((key == 82 || key == 114) && running == false){
     bikeX = 6;
     bikeY = 25;
     bikeX2 = 44;
     bikeY2 = 25;
+    bikeX3 = 25;
+    bikeY3 = 6;
+    bikeX4 = 25;
+    bikeY4 = 44;
     direction = 2;
     direction2 = 1;
+    direction3 = -1;
+    direction4 = 0;
+    p1Dead = false;
+    p2Dead = false;
+    p3Dead = false;
+    p4Dead = false;
   }
 
   if(!running){
@@ -426,6 +428,9 @@ window.addEventListener("keypress", function key(){
   else if(key == 32){
   //  console.log("start");
     running = false;
+  }
+  if(running){
+  //  gameMusic.play();
   }
 });
 
